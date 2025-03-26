8b9ef97b250445af44f23acc333037f2252f97b4
Nakagama Yutaro (VM-EPS/ESW1-JP) <Yutaro.Nakagama@bosch.com>
2025-03-26 18:38:26 +0900

    [modify] modify config.py file

--- project/src/config.py
+++ project/src/config.py
@@ -1,12 +1,8 @@
 import numpy as np
 
 # File paths
-#SUBJECT_LIST_PATH   = './data/external/Aygun2024/subject_list_temp.txt'
-SUBJECT_LIST_PATH   = '../../dataset/mdapbe/subject_list_temp.txt'
-#SUBJECT_LIST_PATH   = './data/Aygun2024/subject_list_temp_ori.txt'
-#DATASET_PATH        = './data/external/Aygun2024/physio'
+SUBJECT_LIST_PATH   = '../../dataset/mdapbe/subject_list.txt'
 DATASET_PATH        = '../../dataset/mdapbe/physio'
-#OUTPUT_CSV_PATH     = './output/csv'
 INTRIM_CSV_PATH     = './data/interim'
 PROCESS_CSV_PATH    = './data/processed'
 MODEL_PKL_PATH    = './model/'
@@ -16,8 +12,6 @@ DATA_PROCESS_CHOICES = ["SvmA", "SvmW", "Lstm", "common"]
 MODEL_CHOICES = ["SvmA", "SvmW", "Lstm", "RF"]  
 
 # Data process parameters
-#WINDOW_SIZE_SEC = 3   # Time window size in seconds
-#STEP_SIZE_SEC = 0.5     # Step size in seconds
 MODEL_WINDOW_CONFIG = {
     "common": {"window_sec": 3, "step_sec": 0.5},
     "SvmA":   {"window_sec": 3, "step_sec": 0.5},
@@ -26,12 +20,8 @@ MODEL_WINDOW_CONFIG = {
 }
 
 SAMPLE_RATE_SIMLSL = 60 # sample rate for simlsl
-#WINDOW_SIZE_SAMPLE_SIMLSL = WINDOW_SIZE_SEC * SAMPLE_RATE_SIMLSL 
-#STEP_SIZE_SAMPLE_SIMLSL = int(STEP_SIZE_SEC * SAMPLE_RATE_SIMLSL) 
 
 SAMPLE_RATE_EEG = 500
-#WINDOW_SIZE_SAMPLE_EEG = WINDOW_SIZE_SEC * SAMPLE_RATE_EEG 
-#STEP_SIZE_SAMPLE_EEG = int(STEP_SIZE_SEC * SAMPLE_RATE_EEG) 
 
 SCALING_FILTER = np.array([0.48296, 0.83652, 0.22414, -0.12941])
 WAVELET_FILTER = np.array([-0.12941, -0.22414, 0.83652, -0.48296])
