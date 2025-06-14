6c9b62f2474fc544e10f6901df9dce0ae07c2423
ynakagama <ynakagama@jaist.ac.jp>
2025-06-15 00:36:45 +0900

    rm comment outs

--- project/bin/train.py
+++ project/bin/train.py
@@ -126,7 +126,6 @@ def main():
         feature_selection_method=args.feature_selection 
     )
 
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
     main()
