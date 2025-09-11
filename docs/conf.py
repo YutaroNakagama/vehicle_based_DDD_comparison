b5fb5f03720ec81b511e075f69ee4c95b35013eb
ynakagama <67034042+YutaroNakagama@users.noreply.github.com>
2025-09-12 02:54:31 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,14 @@ extensions = [
     "sphinx_autodoc_typehints",   # 型ヒントを自動表示
 ]
 
+# conf.py
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_param = True
+napoleon_use_rtype = True
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
