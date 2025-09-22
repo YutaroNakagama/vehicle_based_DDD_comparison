1e80673b65e882dc651bdfbb30a1fb92cdea6c06
Yutaro Nakagama <nkgm1003@gmail.com>
2025-09-22 21:18:56 +0900

    [modify] eng comment in make pretrain group pt

--- misc/make_pretrain_group.py
+++ misc/make_pretrain_group.py
@@ -1,6 +1,6 @@
 # make_general_subjects.py
 with open('subject_list.txt') as fin, open('target_groups.txt') as fin2, open('general_subjects.txt', 'w') as fout:
-    # 1行目（1グループ目）の10人だけ抜く場合
+    # Extract only the first 10 subjects from the first group
     finetune_targets = set(fin2.readline().split())
     for subj in (line.strip() for line in fin if line.strip()):
         if subj not in finetune_targets:
